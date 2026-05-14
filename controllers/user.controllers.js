262869ba5897ae6edf9fcefd5299bbdddf932b1a
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-05-14 20:14:16 +0530

    Define User Routes and Controllers

---
+++ controllers/user.controllers.js
@@ -0,0 +1,108 @@
+import { randomBytes, createHmac } from "node:crypto";
+import { eq } from "drizzle-orm";
+import jwt from "jsonwebtoken";
+
+import db from "../db/index.js";
+import { users } from "../db/schema.js";
+
+const JWT_SECRET = process.env.JWT_SECRET || "secret";
+
+export const me = async (req, res) => {
+  const user = req.user;
+
+  if (!user) {
+    return res.status(401).json({ error: "You are not logged in" });
+  }
+
+  return res.status(200).json({ user });
+};
+
+export const updateUserName = async (req, res) => {
+  const user = req.user;
+
+  if (!user) {
+    return res.status(401).json({ error: "You are not logged in" });
+  }
+
+  const { name } = req.body;
+  await db.update(users).set({ name: name }).where(eq(users.id, user.id));
+
+  return res.json({ status: "success" });
+};
+
+export const signUp = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || name === "") {
+    return res.status(400).json({ error: "name is required" });
+  }
+
+  const [existingUser] = await db
+    .select({
+      email: users.email,
+    })
+    .from(users)
+    .where((table) => eq(table.email, email));
+
+  if (existingUser) {
+    return res
+      .status(400)
+      .json({ error: `user with email ${email} already exists!` });
+  }
+
+  const salt = randomBytes(256).toString("hex");
+  const hashedPassword = createHmac("sha256", salt)
+    .update(password)
+    .digest("hex");
+
+  const [user] = await db
+    .insert(users)
+    .values({
+      name,
+      email,
+      password: hashedPassword,
+      salt,
+    })
+    .returning({ id: users.id });
+
+  return res.status(201).json({ status: "success", data: { userId: user.id } });
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const [existingUser] = await db
+    .select({
+      id: users.id,
+      name: users.name,
+      email: users.email,
+      password: users.password,
+      salt: users.salt,
+    })
+    .from(users)
+    .where((table) => eq(table.email, email));
+
+  if (!existingUser) {
+    return res
+      .status(404)
+      .json({ error: `User with email ${email} does not exists!` });
+  }
+
+  const salt = existingUser.salt;
+  const existingHash = existingUser.password;
+
+  const newHash = createHmac("sha256", salt).update(password).digest("hex");
+
+  if (newHash !== existingHash) {
+    return res.status(401).json({ error: "Invalid passoword!" });
+  }
+
+  // Generate a jwt token
+  const token = jwt.sign({
+    id: existingUser.id,
+    email: existingUser.email,
+    name: existingUser.name
+  }, JWT_SECRET, { expiresIn: '10m' });
+
+  return res.status(200).json({ status: `success`, token: token });
+};
