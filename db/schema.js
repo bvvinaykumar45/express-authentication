1b71a6d8eb15cd0c0d86581b7bda2d9d0f0aecad
bvvinaykumar45 <bvvinaykumar45@gmail.com>
2026-05-18 00:47:08 +0530

    Update Users schema to include user_role

--- db/schema.js
+++ db/schema.js
@@ -1,11 +1,14 @@
-import { pgTable, uuid, varchar, text, timestamp } from "drizzle-orm/pg-core";
+import { pgTable, uuid, varchar, text, timestamp, pgEnum } from "drizzle-orm/pg-core";
+
+export const userRoleEnum = pgEnum('user_role', ['ADMIN', 'USER']);
 
 export const users = pgTable('users', {
   id: uuid('id').primaryKey().defaultRandom(),
   name: varchar('name', {length: 64}).notNull(),
   email: varchar('email', {length: 128}).notNull().unique(),
   password: text('password').notNull(),
-  salt: text('salt').notNull()
+  salt: text('salt').notNull(),
+  role: userRoleEnum().notNull().default("USER")
 });
 
 export const userSessions = pgTable('user_sessions', {
